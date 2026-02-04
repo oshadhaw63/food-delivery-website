dc90fe20914e734ea8a3d56a3a1e47eccda1fc1a
oshadhaw63 <oshadhawijayarathne@yahoo.com>
2026-02-04 12:11:57 +0530

    implement basic navigation bar

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css'
+import { assets } from '../../assets/assets'
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" className="logo"/>
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>about</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt=''/>
+      </div>
+        <div className='navbar-search-icon'>
+            <img src={assets.basket_icon} alt=""/>
+            <div className="dot"></div>
+        </div>
+        <button>sign in</button>
+    </div>
+  )
+}
+
+export default Navbar
