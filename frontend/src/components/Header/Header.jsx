b0cf7af5c1c175b19b6e501599d00c548377623e
oshadhaw63 <oshadhawijayarathne@yahoo.com>
2026-02-06 00:04:41 +0530

    add styles to the header components and animation

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className ='header'>
         <div className='header-contents'>
-            <h2>Order your favourite food here</h2>
+            <h2>Order your <br/>favourite food here</h2>
             <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. </p>
             <button>View Menu</button>
         </div>
