3ac409c53f6b092d8223cb9371e9cfc0a989c9ec
oshadhaw63 <oshadhawijayarathne@yahoo.com>
2026-02-06 12:54:45 +0530

    add food display functionality with context API integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import StoreContextProvider from "./context/StoreContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  </BrowserRouter>,
+);
