c5d6fc8eb8879ccea7b99ca8232a5e4f929d959f
oshadhaw63 <oshadhawijayarathne@yahoo.com>
2026-03-07 12:38:40 +0530

    implement promo code and and cart total features in the cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,13 +1,23 @@
 import React, {useContext} from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
-
   const {cartItems,food_list,removeFromCart}=useContext(StoreContext)
+  const navigate = useNavigate();
 
-
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item] > 0){
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
 
   return (
     <div className='cart'>
@@ -25,22 +35,50 @@ const Cart = () => {
         {food_list.map((item,index)=>{
           if(cartItems[item._id]>0){
             return (
-              <div className="cart-items-title cart-items-item" >
-                <img src={item.image} alt=''/>
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>{cartItems[item._id]*item.price}</p>
-                <p>x</p>
+              <div key={index}>
+                <div className="cart-items-title cart-items-item" >
+                  <img src={item.image} alt=''/>
+                  <p>{item.name}</p>
+                  <p>{item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>{cartItems[item._id]*item.price}</p>
+                  <p onClick={()=>removeFromCart(item._id)}>x</p>
+                </div>
+                <hr />
               </div>
             )
           } 
         })}
+      </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div className="cart-total-details">
+            <div>
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <div>
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <div>
+              <p>Total</p>
+              <p>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</p>
+            </div>
+          </div>
+          <button onClick={()=>navigate('/placeorder')}>PROCEED TO CHECKOUT</button>
         </div>
-    
-          
+        <div className="cart-promocode">
+          <p>If you have a promo code, Enter it here</p>
+          <div className="cart-promocode-input">
+            <input type="text" placeholder='promo code' />
+            <button>Submit</button>
+          </div>
+        </div>
+      </div>
     </div>
-    )
+  )
 }
 
 export default Cart
